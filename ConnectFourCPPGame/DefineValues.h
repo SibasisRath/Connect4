58819f1fcf91e08792ef938651c0c9f9a44d1280
Sibasis Rath <sibasisrath2000@gmail.com>
2024-06-03 11:05:13 +0530

    correcting make file

--- ConnectFourCPPGame/DefineValues.h
+++ ConnectFourCPPGame/DefineValues.h
@@ -3,4 +3,4 @@
 #define ROW 6
 #define COLUMN 7
 #define TOTAL_CONNECT 4
-#define PLACEHOLDER_CHARACTER 'O'
+#define PLACEHOLDER_CHARACTER '-'
--- ConnectFourCPPGame/MainProgram.cpp
+++ ConnectFourCPPGame/MainProgram.cpp
@@ -1,20 +1,4 @@
-﻿#include <iostream>
-#include <string>
-#include <vector>
-#include <limits>
-
-#include "DefineValues.h"
-#include "Player.h"
-#include "Board.h"
-#include "ResultPrinter.h"
-#include "WinningConditionChecker.h"
-#include "HorizontalChecker.h"
-#include "VerticalChecker.h"
-#include "PositiveSlashChecker.h"
-#include "NegetiveSlashChecker.h"
-#include "ResultChecker.h"
-#include "MainGameLoop.h"
-#include "Game.h"
+﻿#include "Game.h"
 
 int main() {
     Game game;
